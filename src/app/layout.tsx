6fb78487d62f9da15ee2c23480abd2ba928125d6
Subin Lee <ewaterbinn@gmail.com>
2024-11-17 04:30:35 +0900

    style: font 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,26 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.scss';
 
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  fallback: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'sans-serif',
+  ],
+});
+
 export const metadata: Metadata = {
   title: '우리의 이름 궁합은?',
   description: '재미로 보는 이름 궁합',
+  icons: '/assets/cat.png',
 };
 
 export default function RootLayout({
@@ -12,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ko'>
+    <html lang='ko' className={pretendard.className}>
       <body>{children}</body>
     </html>
   );
