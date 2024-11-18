63a2ec50f4e6d1a6c245beaec38a12bac073ba42
Subin Lee <ewaterbinn@gmail.com>
2024-11-19 00:46:13 +0900

    fix: 이미지 경로 절대경로로 수정

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,26 +2,30 @@ export default function Head() {
   return (
     <>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <link rel='icon' href='/assets/cat.png' />
       <link
         rel='canonical'
         href='https://name-compatibility-test.vercel.app/'
       />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <meta property='og:type' content='website' />
-      <meta property='og:title' content={`이름 궁합 테스트`} />
-      <meta property='og:description' content='우리의 이름 궁합은?' />
-      <meta property='og:imgae' content='/assets/share_image.png' />
-      <meta property='og:site_name' content={'이름 궁합 테스트'} />
+      <meta property='og:title' content='우리의 이름 궁합은?' />
+      <meta property='og:description' content='재미로 보는 이름 궁합' />
+      <meta
+        property='og:image'
+        content='https://name-compatibility-test.vercel.app/assets/share_image.png'
+      />
+      <meta property='og:site_name' content='이름 궁합 테스트' />
       <meta
         property='og:url'
-        content={'https://name-compatibility-test.vercel.app/'}
+        content='https://name-compatibility-test.vercel.app/'
       />
-      {/* x */}
-      <meta name='twitter:card' content='card' />
-      <meta name='twitter:title' content='이름 궁합 테스트' />
-      <meta name='twitter:description' content='우리의 이름 궁합은?' />
-      <meta property='twitter:imgae' content='/assets/share_image.png' />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:title' content='우리의 이름 궁합은?' />
+      <meta name='twitter:description' content='재미로 보는 이름 궁합' />
+      <meta
+        name='twitter:image'
+        content='https://name-compatibility-test.vercel.app/assets/share_image.png'
+      />
     </>
   );
 }
