0e3ba43c15b5feeb6669fbf077c61db6f429c2b1
Subin Lee <ewaterbinn@gmail.com>
2024-11-28 16:14:17 +0900

    feat: 안내 버튼 추가

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -8,13 +8,19 @@ import { useEffect, useState } from 'react';
 
 import Arrow from '/public/assets/icon_arrow_s.svg';
 
-import { Button } from '@waterbin/ui-kit';
 import { useRouter } from 'next/navigation';
 
 import styles from './Main.module.scss';
 import { koreanRegex } from '@/constant';
 
 import KakaoAdFit from '@/lib/KakaoAdFit';
+
+import {
+  Button,
+  Popover,
+  PopoverContent,
+  PopoverHandler,
+} from '@waterbin/ui-kit';
 
 export const Main = () => {
   const router = useRouter();
@@ -89,16 +95,49 @@ export const Main = () => {
           이름을 입력해 주세요. (2~4자리의 한글 이름만 가능합니다.)
         </span>
 
-        <Button
-          className={cx('button-submit')}
-          onClick={handleSubmit}
-          variant='iconText'
-          size='lg'
-          disabled={!isValid}
-          icon={<Arrow />}
-        >
-          궁합 보기
-        </Button>
+        <div className={cx('button-wrap')}>
+          <Button
+            className={cx('button-submit')}
+            onClick={handleSubmit}
+            variant='iconText'
+            size='lg'
+            disabled={!isValid}
+            icon={<Arrow />}
+          >
+            궁합 보기
+          </Button>
+
+          <Popover>
+            <PopoverHandler>
+              <Button size='sm' color='teal'>
+                💙 안내사항 💙
+              </Button>
+            </PopoverHandler>
+            <PopoverContent placement='right'>
+              <ul className={cx('info-wrap')}>
+                <li className={cx('info-list')}>
+                  💙 궁합 볼 사람이 없다고요?
+                  <p className={cx('info-content')}>
+                    연예인이랑 하시는 건 어떤지..
+                  </p>
+                </li>
+                <li className={cx('info-list')}>
+                  💙 버그를 발견하셨다고요?
+                  <p className={cx('info-content')}>
+                    묻어 ....... 가 아니고
+                    <br />
+                    <a href='mailto:ewaterbinn@gmail.com'>
+                      ewaterbinn@gmail.com
+                    </a>
+                    으로
+                    <br />
+                    버그에 대해 알려주세요!
+                  </p>
+                </li>
+              </ul>
+            </PopoverContent>
+          </Popover>
+        </div>
 
         <span className={cx('copyright')}>
           ©copyright waterbinn | made by waterbinn
