fe798903f52c13c5030d71cf6b890c70bbccc205
Subin Lee <ewaterbinn@gmail.com>
2025-02-04 16:12:06 +0900

    docs: update meta tag

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,31 +1,25 @@
 export default function Head() {
+  const title = '우리의 이름 궁합은? | 이름 궁합 테스트';
+  const desc =
+    '이름 궁합 테스트로 당신과 상대의 이름을 입력해 궁합을 확인해보세요! 간단하고 재미있는 이름 궁합 서비스입니다.';
+  const siteUrl = 'https://name-compatibility-test.vercel.app/';
+  const imageUrl = `${siteUrl}assets/share_image.png`;
+
   return (
     <>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <link
-        rel='canonical'
-        href='https://name-compatibility-test.vercel.app/'
-      />
+      <link rel='canonical' href={siteUrl} />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <meta property='og:type' content='website' />
-      <meta property='og:title' content='우리의 이름 궁합은?' />
-      <meta property='og:description' content='재미로 보는 이름 궁합' />
-      <meta
-        property='og:image'
-        content='https://name-compatibility-test.vercel.app/assets/share_image.png'
-      />
-      <meta property='og:site_name' content='이름 궁합 테스트' />
-      <meta
-        property='og:url'
-        content='https://name-compatibility-test.vercel.app/'
-      />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={desc} />
+      <meta property='og:image' content={imageUrl} />
+      <meta property='og:site_name' content={title} />
+      <meta property='og:url' content={siteUrl} />
       <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:title' content='우리의 이름 궁합은?' />
-      <meta name='twitter:description' content='재미로 보는 이름 궁합' />
-      <meta
-        name='twitter:image'
-        content='https://name-compatibility-test.vercel.app/assets/share_image.png'
-      />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={desc} />
+      <meta name='twitter:image' content={imageUrl} />
     </>
   );
 }
